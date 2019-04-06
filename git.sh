3c863aabc91a6ffd971faa7f03065b3d49480c8f
Javier Islas <javier.i@inspireinnovations.com>
2019-04-05 18:50:03 -0600

    Se agrega el comando reset

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 # Todos los archivos pueden estar en 2 estados:
 # Tracked   => No modificados, Modificados, en Stage.
-# Untracked => Cualquier archivo que aun no esta en Stage.
+# Untracked => Cualquier archivo que aun no ha estado en Stage.
 
 # Asignar variables globales o locales al repositorio.
 git config --global user.name "Javier Islas"
@@ -18,8 +18,11 @@ git status
 # Deshacer cambios locales.
 git checkout *.txt
 
-# Agregar archivos al stage para commit.
+# Agregar archivos al Stage para commit.
 git add *.txt
+
+# Pasa archivos de Stage a Modified.
+git reset HEAD *.txt
 
 # Hacer commit en el repositorio local.
 git commit -m "Mensaje"
